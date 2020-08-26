423d13e24390e2f6f891a76c88ae088b02aa7268
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-26 12:44:27 +0200

    Clearerd out init

--- meta_models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .meta_layers import (
-    DenseMetaLayer, Conv1DMetaLayer, Conv2DMetaLayer, Conv3DMetaLayer,
-    DenseRectangularMetaLayer, Conv2DRectangularMetaLayer, Conv3DRectangularMetaLayer, Conv1DRectangularMetaLayer,
-    FlattenMetaLayer, HeadMetaLayer, InputMetaLayer
-)
-from .meta_models import CNN1DMetaModel, CNN2DMetaModel, CNN3DMetaModel, MMNNMetaModel, FFNNMetaModel
