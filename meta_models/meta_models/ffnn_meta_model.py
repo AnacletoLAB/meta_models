819532c75c73e8501fcec7a477a53c8cf47d0c7b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-08-26 11:47:22 +0200

    Updated UML schema

--- meta_models/meta_models/ffnn_meta_model.py
+++ meta_models/meta_models/ffnn_meta_model.py
@@ -16,6 +16,28 @@ class FFNNMetaModel(MetaModel):
 
     The class is also meant to be used as a sub-module in the context of a
     multi-modal neural network.
+
+    Private members
+    -----------------------------
+    _input_shape: Union[int, Tuple[int]] = None,
+        The input shape of the layer.
+        If an integer is provided it will be converted to a tuple.
+        If no input_shape is provided then an InputMetaLayer must
+        be passed in the structure method.
+    _blocks: int = 4,
+        Number of blocks of the network.
+    _activation: str = "relu",
+        The activation function to use for the layer.
+    _output_units: int = 1,
+        Number of neurons of the output layer.
+    _meta_layer_kwargs: Dict = None,
+        Keyword arguments to pass to the builder of Dense Meta Layers.
+    _input_name: str = None,
+        Name of the input layer. This value is often used in the context
+        of multimodal neural networks, otherwise is pretty meaningless
+        if not for help in readability in the model summary dump.
+    _headless: bool = False,
+        Wethever to return the model as headless.
     """
 
     def __init__(
