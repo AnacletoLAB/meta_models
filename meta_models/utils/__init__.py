6738b58199370eae568225b8728502433ef4d3c5
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-18 10:41:58 +0100

    Exposed get_gpu_number

--- meta_models/utils/__init__.py
+++ meta_models/utils/__init__.py
@@ -1,5 +1,5 @@
 from .stratified_holdouts import stratified_holdouts
-from .subgpu_training import get_minimum_gpu_rate_per_trial, enable_subgpu_training
+from .subgpu_training import get_minimum_gpu_rate_per_trial, enable_subgpu_training, get_gpu_number
 from .distributions import distributions
 from .tune_patches import patch_global_checkpoints_interval
 
@@ -7,6 +7,7 @@ __all__ = [
     "stratified_holdouts",
     "get_minimum_gpu_rate_per_trial",
     "enable_subgpu_training",
+    "get_gpu_number",
     "distributions",
     "patch_global_checkpoints_interval"
 ]
